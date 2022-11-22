4b67865cfbbda0963bfc855ac10acdfa9d456874
Dawntraoz <albasilventefu@gmail.com>
2022-11-22 17:54:15 +0100

    feat: exercise 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+				accessToken: 'QiiuldeZsletspIAOYLeSwtt',
+			}
+    ],
+	  '@nuxtjs/tailwindcss'
+  ],
 })
